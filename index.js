0f7a8a90ec76ba809ba8cb6a184459c894045785
Santhosh V <santhosh.v1@flipkart.com>
2023-02-21 15:51:29 +0530

    connect to db

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
+const mongoose = require("mongoose")
+require("dotenv").config();
 
 
 const app = express();
@@ -8,5 +10,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors())
 
 
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGODB_CONNECT)
+    .then(res => {
+        app.listen(4000)
+    })
+    .catch(err => {
+        console.log(err)
+    })
 
-app.listen(4000)
+
+
