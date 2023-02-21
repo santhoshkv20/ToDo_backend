99a1e0ad2094f66d44ab61df1c835d497ffe3e41
Santhosh V <santhosh.v1@flipkart.com>
2023-02-21 21:12:45 +0530

    added validation for input

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,11 +1,13 @@
 const bcrypt = require("bcryptjs")
+const { validationResult } = require("express-validator")
 const User = require("../Schema/User")
 
-exports.postSignin = (req,res)=>{
-const {email,password} = req.body
+exports.postSignin = (req, res) => {
+    const { email, password } = req.body
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.json({ error: errors.array()[0].msg });
 
     User.findOne({ email }).then(user => {
-        console.log(user)
         if (!user) return res.status(422).json({ "STATUS": "Failure", "message": "Email or password does not match" })
         bcrypt.compare(password, user.password).then(doMacth => {
             if (doMacth) {
@@ -24,6 +26,8 @@ const {email,password} = req.body
 
 exports.postSignup = (req, res) => {
     const { name, email, password } = req.body
+    const errors = validationResult(req);
+    if(!errors.isEmpty())return res.json({error:errors.array()[0].msg});
     bcrypt.hash(password, 12).then(hashedPassword => {
         let user = new User({ name: name, email: email, password: hashedPassword })
         user.save().then(user => {
--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const { postSignup, postSignin } = require("../Controller/UserController");
 const userRouter = express.Router();
+const { check, body } = require("express-validator")
 
-userRouter.post("/signin",postSignin)
-userRouter.post("/signup",postSignup)
+userRouter.post("/signin", [
+    check("email", "Email is not valid").isEmail().trim()
+], postSignin)
+
+userRouter.post("/signup", [
+    check("name", "Name must be 4 characters long and only letters").isLength({ min: 4 }).isString().trim(),
+    check("email").isEmail().withMessage("Email is not valid").trim(),
+    check("password").isLength({ min: 8 }).withMessage("Password must be min 8 character long").trim()
+
+], postSignup)
+userRouter.post("/addTask")
 
 
 module.exports = userRouter
